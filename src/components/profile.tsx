00961e813a6829168fbd86773c90d53dbb0f1f18
Sashok PRO <suvor.202@gmail.com>
2023-04-06 12:34:38 +0300

    social

---
+++ src/components/profile.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+
+export const Profile = () => {
+    return <div className="content">
+        <div><img src="https://www.kartinki24.ru/uploads/gallery/main/196/kartinki24_ru_forest_224.jpg" alt=""/>
+        </div>
+        <div>
+            <div><img
+                src="https://65.mchs.gov.ru/uploads/resize_cache/news/2021-08-25/pravila-povedeniya-pri-vstreche-s-medvedem_1629847892112633638__800x800.jpg"
+                alt=""/></div>
+            <div>
+                <div>Sasha D.</div>
+                <div>Date of Birth: April 22</div>
+                <div>Сity: st.Petersburg</div>
+                <div>Unknown</div>
+                <div>Unknown</div>
+            </div>
+        </div>
+        <div>
+            My posts
+            <div>
+                New post
+            </div>
+            <div>
+                <div>Post 1</div>
+                <div>Post 2</div>
+            </div>
+        </div>
+    </div>
+}
